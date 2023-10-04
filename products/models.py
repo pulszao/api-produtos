f52459ddcaf859dce92a9535d5a5e120ebfb028e
Lucas Pulsz <lucaspulsz@gmail.com>
2023-10-04 11:57:41 -0300

    Products -> Ajustando models

--- products/models.py
+++ products/models.py
@@ -2,6 +2,15 @@ from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
 
+class ProductDTO(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(verbose_name='Nome do Produto', max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Preço do Produto')
+    description = models.TextField(verbose_name='Descrição do Produto')
+    inStock = models.BooleanField()
+    quantity = models.IntegerField(verbose_name='Quantidade Disponível do Produto')
+
+
 class ProductInputDTO(models.Model):
     name = models.CharField(verbose_name='Nome do Produto', max_length=120, validators=[
         MinLengthValidator(5, 'Nome deve conter no minimo 5 caracteres.')
@@ -17,8 +26,21 @@ class ProductInputDTO(models.Model):
         MinValueValidator(0, 'Quantidade deve ser de no minimo 0.')
     ])
 
+    def save(self, *args, **kwargs):
+        # save ProductDTO
+        product = ProductDTO()
+        product.name = self.name
+        product.price = self.price
+        product.description = self.description
+        product.inStock = self.inStock
+        product.quantity = self.quantity
+        product.save()
+
+        super().save(*args, **kwargs)
+
 
 class ProductUpdateDTO(models.Model):
+    product = models.ForeignKey(ProductDTO, on_delete=models.CASCADE, related_name='base_product')
     name = models.CharField(verbose_name='Nome do Produto', max_length=120, validators=[
         MinLengthValidator(5, 'Nome deve conter no minimo 5 caracteres.')
     ])
@@ -32,12 +54,14 @@ class ProductUpdateDTO(models.Model):
         MinValueValidator(0, 'Quantidade deve ser de no minimo 0.')
     ])
 
+    def save(self, *args, **kwargs):
+        # update ProductDTO
+        product = ProductDTO.objects.get(id=self.product.id)
+        product.name = self.name
+        product.price = self.price
+        product.description = self.description
+        product.quantity = self.quantity
+        product.save()
 
-class ProductDTO(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(verbose_name='Nome do Produto', max_length=120)
-    price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Preço do Produto')
-    description = models.TextField(verbose_name='Descrição do Produto')
-    inStock = models.BooleanField()
-    quantity = models.IntegerField(verbose_name='Quantidade Disponível do Produto')
+        super().save(*args, **kwargs)
 
