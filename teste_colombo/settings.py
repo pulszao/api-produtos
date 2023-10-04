e79f07e8d88971256729a16f335e4de4d3ce8730
Lucas Pulsz <lucaspulsz@gmail.com>
2023-10-04 16:26:15 -0300

    Products -> Ajustando settings.py

--- teste_colombo/settings.py
+++ teste_colombo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-av3q_mxbzrt3u)0rt_f3d268sl#8#q9=5f^*6r@^m+5hvdny%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
